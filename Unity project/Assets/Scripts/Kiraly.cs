b6729179a686f3413e410723ac5f9ab9dd33f6de
DESKTOP-1P0OM2L\Peti <barath.peterg@gmail.com>
2022-06-18 09:40:19 +0200

    Upload project

---
+++ Unity project/Assets/Scripts/Kiraly.cs
@@ -0,0 +1,89 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Kiraly : Chessmans
+{
+    public override bool[,] PossibleMove()
+    {
+        bool[,] r = new bool[8, 8];
+        Chessmans c;
+        int i, j;
+        //Top Side
+        i = CurrentX - 1;
+        j = CurrentY + 1;
+        if (CurrentY != 7)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                if (i >= 0 || i < 8)
+                {
+                    c = BoardManager.Instance.Chessmans[i, j];
+                    if (c == null)
+                    {
+                        r[i, j] = true;
+                    }
+                    else if (isWhite != c.isWhite)
+                    {
+                        r[i, j] = true;
+                    }
+                    i++;
+                }
+            }
+
+
+        }
+        //Down Side
+        i = CurrentX - 1;
+        j = CurrentY - 1;
+        if (CurrentY != 0)
+        {
+            for (int k = 0; k < 3; k++)
+            {
+                if (i >= 0 || i < 8)
+                {
+                    c = BoardManager.Instance.Chessmans[i, j];
+                    if (c == null)
+                    {
+                        r[i, j] = true;
+                    }
+                    else if (isWhite != c.isWhite)
+                    {
+                        r[i, j] = true;
+                    }
+                    i++;
+                }
+
+
+            }
+        }
+
+        //Middle Left
+        if (CurrentX!=0)
+        {
+            c = BoardManager.Instance.Chessmans[CurrentX - 1, CurrentY];
+            if (c==null)
+            {
+                r[CurrentX - 1, CurrentY]=true;
+            }
+            else if (isWhite!=c.isWhite)
+            {
+                r[CurrentX - 1, CurrentY] = true;
+            }
+        }
+        //Middle Right
+        if (CurrentX != 0)
+        {
+            c = BoardManager.Instance.Chessmans[CurrentX + 1, CurrentY];
+            if (c == null)
+            {
+                r[CurrentX + 1, CurrentY] = true;
+            }
+            else if (isWhite != c.isWhite)
+            {
+                r[CurrentX + 1, CurrentY] = true;
+            }
+        }
+        return r;
+    }
+}
